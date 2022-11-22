c0bc3b22a5f8ce060e345ad0e62bc12e1209c495
khamrayev5636 <khamrayev5636@gmail.com>
2022-11-22 23:20:43 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const elForm = document.querySelector(".form");
+const elInput = elForm.querySelector(".form__input-js");
+const elSelect = elForm.querySelector(".form__select-js");
+const elOutput = document.querySelector(".output-js");
+const elBtn = document.querySelector(".btn-js")
+
+elForm.addEventListener("keyup" , (evt)=> {
+    evt.preventDefault();
+
+    const elInputValue = elInput.value.trim();
+    const elSelectValue = elSelect.value.trim();
+
+    elOutput.textContent = elInputValue * elSelectValue;
+
+});
+
+
+
